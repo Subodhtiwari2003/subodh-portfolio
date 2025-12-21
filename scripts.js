1efb0250a460bb794197aaa786b09fd4cea6a952
Subodh Tiwari <kumarsubodh150403@gmail.com>
2025-12-21 15:51:52 +0530

    final commit

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 const toggle = document.getElementById("theme-toggle");
-let dark = true;
 
-toggle.onclick = () => {
-  document.body.style.background = dark ? "#f8fafc" : "#0f172a";
-  document.body.style.color = dark ? "#020617" : "#e5e7eb";
-  dark = !dark;
-};
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+});
+
